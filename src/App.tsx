765d2eb5e0cc7783d945b6c53a37332703fc0a08
Vandamchik <cherepcock@gmail.com>
2023-10-11 19:33:08 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './App.module.scss';
+
+export const App: React.FC = () => {
+  return (
+    <h1 className={styles.text}>
+      Hello
+    </h1>
+  )
+}
+
